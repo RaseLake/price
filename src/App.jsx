5587ea3728faeac88e93ad83de33856f1cddc03b
RaseLake <raselahamed2911@gmail.com>
2023-09-25 11:44:14 +0600

    working on 44-3 videos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import DaisyNav from './components/daisynav/DaisyNav'
+import NavBar from './Navbar/NavBar';
 
 function App() {
   
 
   return (
     <>
-      
+      <NavBar></NavBar>
+      <DaisyNav></DaisyNav>
       <h1 className='text-7xl bg-rose-400' >Vite + React</h1>
 
       
---
+++ src/components/Link/Link.jsx
@@ -0,0 +1,11 @@
+import PropTypes from 'prop-types';
+
+const Link = ({route}) => {
+    return (
+        <li className='mr-10'
+        >
+                <a href={route.path}>{route.name}</a></li>
+    );
+};
+
+export default Link;
